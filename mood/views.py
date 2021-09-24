241b51fa8f35950ecaf76c30a861dbfd2e8d25c6
Courtney Zhan <qinmengzhan@gmail.com>
2021-09-24 11:51:10 +1000

    Error message on others' moods

--- mood/views.py
+++ mood/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .models import Mood
 from django.views.generic import CreateView, DetailView, ListView
 
@@ -27,9 +30,21 @@ class MoodListView(CustomLoginRequiredMixin, ListView):
     context_object_name = 'moods'
     ordering = ['-date_posted']
 
+    def get_queryset(self):
+        return Mood.objects.filter(author=self.request.user)
+
 class MoodDetailView(CustomLoginRequiredMixin, DetailView):
     model = Mood
     login_url = "login"
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        objects_filter = Mood.objects.filter(author=self.request.user).filter(pk=pk)
+        if len(objects_filter) == 0: # not found
+            messages.add_message(self.request, messages.WARNING,
+                                 "You don't have permission to access that mood")
+            # TODO redirect to mood list page
+        return objects_filter
 
 class MoodCreateView(CustomLoginRequiredMixin, CreateView):
     # Redirect if not authenticated
