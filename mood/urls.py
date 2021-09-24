80170505d96ffbd4f6f054837ff3a5def4349f2f
Courtney Zhan <qinmengzhan@gmail.com>
2021-09-24 10:52:17 +1000

    Add server to allowed hosts

--- mood/urls.py
+++ mood/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('moods/', MoodListView.as_view(), name='mood-home'),
     path('moods/<int:pk>', MoodDetailView.as_view(), name='mood-detail'),  # pk primary key, int integer
     path('moods/new/', MoodCreateView.as_view(), name='mood-create'),
-    path('charts/', views.display, name='mood-display'),
+    path('calendar/', views.display, name='mood-display'),
 ]
